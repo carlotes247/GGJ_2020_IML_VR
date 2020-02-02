0d51927a45318e1a037496dea0488d47174adf19
Carlotes_Desktop_YorkUni <carlosglesdiaz@gmail.com>
2020-02-02 17:10:24 +0000

    Created simple script that pipes UI data from ML Controller to Unity UI

---
+++ Assets/Scripts/IMLPipeUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using InteractML;
+
+/// <summary>
+/// Pipes data and logic between InteractML and the Unity UI system
+/// </summary>
+public class IMLPipeUI : MonoBehaviour
+{
+
+    // Text to update the UI
+    [Header("UI Setup")]
+    public Text ExamplesText;
+    public Text TrainingStatusText;
+
+    [Header("InteractML Setup")]
+    public IMLComponent MLComponent; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MLComponent)
+        {
+            // Update UI
+            // Go through each model node (IML Config)
+            foreach (var modelNode in MLComponent.IMLConfigurationNodesList)
+            {
+                if (modelNode != null)
+                {
+                    // Collected Examples
+                    if (ExamplesText)
+                    {
+                        ExamplesText.text = modelNode.TotalNumTrainingData.ToString();
+                    }
+                    // Training Status
+                    if (TrainingStatusText)
+                    {
+                        TrainingStatusText.text = modelNode.ModelStatus.ToString();
+                    }
+                }
+            }
+        }
+    }
+
+}
